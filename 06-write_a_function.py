ac2da45eddd3b8d1874c8c48acc97cc6dac7a6d0
Md. Shohanur Rahaman <80113547+shohan139@users.noreply.github.com>
2023-03-24 11:12:34 +0600

    Update 06-write_a_function.py
    description

--- 06-write_a_function.py
+++ 06-write_a_function.py
@@ -1,14 +1,16 @@
+# a function which returns true if the year is a leap year otherwise returns false
 def is_leap(year):
     leap = False
     
-    if year%4 == 0:
+    if year%100 == 0 and year%400 == 0:
         leap = True
-    elif year%100 == 0 and year%400 == 0:
-        leap = True
+    elif year%4 == 0:
+        if year%100 !=0 and year%400 != 0:
+            leap = True
     else:
         leap = False
-    
     return leap
+        
 
 year = int(input())
 print(is_leap(year))
