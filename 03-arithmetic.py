9674cb8b83c693148da96b39699efabf68a153b1
Md. Shohanur Rahaman <80113547+shohan139@users.noreply.github.com>
2023-03-24 18:51:51 +0600

    Create 03-arithmetic.py

---
+++ 03-arithmetic.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a+b) # addition (+) operator does the addition operation
+    print(a-b) # subtraction (-) operator does the subtraction operation
+    print(a*b) # multiplication (*) operator does the multiplication
